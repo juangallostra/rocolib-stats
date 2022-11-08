c3b9c0fa47c2286effe156bc0f229af2cd335382
juangallostra <juangallostra@gmail.com>
2022-11-08 17:03:51 +0100

    feature: add page to explore DDBB

--- src/components/TreeView.tsx
+++ src/components/TreeView.tsx
@@ -6,10 +6,15 @@ import {
   MultiBackend,
   getBackendOptions,
 } from "@minoru/react-dnd-treeview";
-import SampleData from "./sample.json";
 
-export const TreeView = () => {
-  const [treeData, setTreeData] = useState<NodeModel[]>(SampleData);
+import { IconChevronRight, IconChevronDown } from "@tabler/icons";
+import "./TreeView.css";
+export interface TreeViewProps {
+  nodes: NodeModel[]
+}
+
+export const TreeView = ({nodes}: TreeViewProps) => {
+  const [treeData, setTreeData] = useState<NodeModel[]>(nodes);
   const handleDrop = (newTreeData: NodeModel[]) => setTreeData(newTreeData);
 
   return (
@@ -19,9 +24,10 @@ export const TreeView = () => {
         rootId={0}
         onDrop={handleDrop}
         render={(node, { depth, isOpen, onToggle }) => (
-          <div style={{ marginLeft: depth * 10 }}>
+          <div style={{ marginLeft: depth * 10, display: 'flex' }}>
             {node.droppable && (
-              <span onClick={onToggle}>{isOpen ? "[-]" : "[+]"}</span>
+              // <span onClick={onToggle}>{isOpen ? "[-]" : "[+]"}</span>
+              <span onClick={onToggle}>{isOpen ? <IconChevronDown/> : <IconChevronRight/>}</span>
             )}
             {node.text}
           </div>
--- src/pages/DataExplorer.tsx
+++ src/pages/DataExplorer.tsx
@@ -1,41 +1,86 @@
-import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { getGyms, getGymWalls, getProblems } from '../api';
 import { TreeView } from '../components';
 import { AppShellLayout } from '../layout';
+import { NodeModel } from "@minoru/react-dnd-treeview";
+import { Loader, Text } from '@mantine/core';
 
 interface DataExplorerProps {
     logout: Function;
     username: string;
 }
 
+
 // Get gyms, walls and problems
-const getTree = () => {
-    getGyms().then(
-        gymArray => gymArray.map(
-            g => getGymWalls(g.id).then(
-                w => console.log(w)
-            )
-        )
-    );
-    return true;
+const getTree = async (): Promise<any> => {
+    const gyms = await getGyms();
+
+    let dataTree: any[] = [];
+    let id = 1;
+
+    let NameToIdMap = new Map<string, number>();
+
+    gyms.forEach(g => {
+        dataTree.push({
+            id: id,
+            parent: 0,
+            droppable: true,
+            text: g.name,
+        });
+        NameToIdMap.set(g.name, id);
+        id++;
+    });
+
+    for (const gym of gyms) {
+        const gymWalls = await getGymWalls(gym.id);
+        for (const wall of gymWalls) {
+            console.log(`Adding wall ${wall.name} to gym ${gym.name} that has tree id ${NameToIdMap.get(gym.name)}`);
+            // for each wall we should get its parent from the gym name matches
+            // Check the id in the
+            dataTree.push({
+                id: id,
+                parent: NameToIdMap.get(gym.name),
+                droppable: false,
+                text: wall.name,    
+            });
+            id++;
+        }
+
+        const gymProblems = await getProblems(gym.id);
+        // Add to tree
+    }
+
+    console.log(dataTree);
+
+    return dataTree;
 }
 
 
-export const DataExplorer = ({logout, username}: DataExplorerProps) => {
+export const DataExplorer = ({ logout, username }: DataExplorerProps) => {
 
     const ddbbQueryKey = 'ddbbData';
 
+    const { isLoading, error, data } = useQuery<boolean, unknown, NodeModel[]>([ddbbQueryKey], () => getTree());
 
-
-    const { isLoading, error, data } = useQuery<boolean, unknown, any>([ddbbQueryKey], () => getTree())
-
-
-
+    // build the tree somehow 
+    if (isLoading) {
+        return (
+            <AppShellLayout username={username} logout={logout}>
+                <h2>Dashboard</h2>
+                <div className='loader-wrapper'>
+                    <Loader size={'xl'} />
+                </div>
+            </AppShellLayout>
+        )
+    }
+    if (error) {
+        <Text>Error loading data</Text>
+    }
     return (
         <AppShellLayout username={username} logout={logout}>
             <h2>Database Data Explorer</h2>
-            <TreeView></TreeView>
+            <TreeView nodes={data!}></TreeView>
         </AppShellLayout>
     )
 }
