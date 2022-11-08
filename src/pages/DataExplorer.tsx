154e9f74ceba0194ee4a64be0853545fb04defe5
juangallostra <juangallostra@gmail.com>
2022-11-08 17:56:33 +0100

    feature: droppable if node has children

--- src/pages/DataExplorer.tsx
+++ src/pages/DataExplorer.tsx
@@ -16,43 +16,55 @@ interface DataExplorerProps {
 const getTree = async (): Promise<any> => {
     const gyms = await getGyms();
 
-    let dataTree: any[] = [];
+    let dataTree: NodeModel[] = [];
     let id = 1;
 
-    let NameToIdMap = new Map<string, number>();
+    let GymNameToIdMap = new Map<string, number>();
+    let WallNameToIdMap = new Map<string, number>();
 
     gyms.forEach(g => {
         dataTree.push({
             id: id,
             parent: 0,
-            droppable: true,
+            droppable: false,
             text: g.name,
         });
-        NameToIdMap.set(g.name, id);
+        GymNameToIdMap.set(g.name, id);
         id++;
     });
 
     for (const gym of gyms) {
         const gymWalls = await getGymWalls(gym.id);
         for (const wall of gymWalls) {
-            console.log(`Adding wall ${wall.name} to gym ${gym.name} that has tree id ${NameToIdMap.get(gym.name)}`);
             // for each wall we should get its parent from the gym name matches
-            // Check the id in the
             dataTree.push({
                 id: id,
-                parent: NameToIdMap.get(gym.name),
+                parent: GymNameToIdMap.get(gym.name)!,
                 droppable: false,
                 text: wall.name,    
             });
+            // Add new map
+            WallNameToIdMap.set(wall.image, id);
+            // Make parent droppable
+            const parentIdx = dataTree.findIndex((node => node.id === GymNameToIdMap.get(gym.name)));
+            dataTree[parentIdx].droppable = true;
             id++;
         }
 
         const gymProblems = await getProblems(gym.id);
-        // Add to tree
+        for (const problem of gymProblems) {
+            dataTree.push({
+                id: id,
+                parent: WallNameToIdMap.get(problem.section)!,
+                droppable: false,
+                text: problem.name,    
+            });
+            // Make parent droppable
+            const parentIdx = dataTree.findIndex((node => node.id === WallNameToIdMap.get(problem.section)));
+            dataTree[parentIdx].droppable = true;            
+            id++;
+        }
     }
-
-    console.log(dataTree);
-
     return dataTree;
 }
 
